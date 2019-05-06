4006721dbfbef1d09e476f0b6b22b13cf8ab5694
MrHidalgo <Luckyhidalgo@gmail.com>
2019-05-06 13:39:02 +0300

    Header - full adaptive.

--- dest/js/app.js
+++ dest/js/app.js
@@ -16,22 +16,48 @@
 * */
 
 /**
+ * @name initHamburger
+ *
+ * @description Init hamburger logic with animated
+ */
+var initHamburger = function initHamburger() {
+
+  var btn = document.querySelector("[hamburger-js]"),
+      hideScrollContainer = document.querySelectorAll("html, body"),
+      mobileContainer = document.querySelector("[mobile-block-js]");
+
+  /**
+    * @description
+   */
+  btn.addEventListener("click", function (ev) {
+    var elem = ev.currentTarget;
+
+    elem.classList.toggle("is-active");
+    mobileContainer.classList.toggle("is-open");
+
+    hideScrollContainer.forEach(function (val, idx) {
+      val.classList.toggle("is-hideScroll");
+    });
+  });
+};
+
+/**
  * @name initPreventBehavior
  *
  * @description
  */
 var initPreventBehavior = function initPreventBehavior() {
 
-	var link = document.querySelectorAll("a");
+  var link = document.querySelectorAll("a");
 
-	link.forEach(function (val, idx) {
+  link.forEach(function (val, idx) {
 
-		val.addEventListener("click", function (e) {
-			if (val.getAttribute("href") === "#") {
-				e.preventDefault();
-			}
-		});
-	});
+    val.addEventListener("click", function (e) {
+      if (val.getAttribute("href") === "#") {
+        e.preventDefault();
+      }
+    });
+  });
 };
 
 /**
@@ -41,7 +67,7 @@ var initPreventBehavior = function initPreventBehavior() {
  */
 var initSvg4everybody = function initSvg4everybody() {
 
-	svg4everybody();
+  svg4everybody();
 };
 
 /**
@@ -51,113 +77,114 @@ var initSvg4everybody = function initSvg4everybody() {
  */
 var initWebFontLoader = function initWebFontLoader() {
 
-	/**
-   * @description
-  */
-	WebFont.load({
-		google: {
-			families: ['Lora:400,700', 'Open+Sans:300,400,600,700,800']
-		}
-	});
+  /**
+    * @description
+   */
+  WebFont.load({
+    google: {
+      families: ['Lora:400,700', 'Open+Sans:300,400,600,700,800']
+    }
+  });
 
-	/**
-   * @description
-  */
-	// const WebFontConfig = {
-	//   custom: {
-	//     families: [
-	//       'Lato:n1,n3,n4,n5,n6,n7,n9'
-	//     ]
-	//   }
-	// };
+  /**
+    * @description
+   */
+  // const WebFontConfig = {
+  //   custom: {
+  //     families: [
+  //       'Lato:n1,n3,n4,n5,n6,n7,n9'
+  //     ]
+  //   }
+  // };
 };
 
 /**
  * @description Document DOM ready.
  */
 (function () {
-	/*
- * =============================================
- * CALLBACK :: start
- * ============================================= */
-	var initPricingTabs = function initPricingTabs() {
-		var _btns = document.getElementsByClassName('pricing__tabs');
+  /*
+  * =============================================
+  * CALLBACK :: start
+  * ============================================= */
+  var initPricingTabs = function initPricingTabs() {
+    var _btns = document.getElementsByClassName('pricing__tabs');
 
-		var _iteratorNormalCompletion = true;
-		var _didIteratorError = false;
-		var _iteratorError = undefined;
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
 
-		try {
-			for (var _iterator = _btns[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-				var _el = _step.value;
+    try {
+      for (var _iterator = _btns[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var _el = _step.value;
 
-				_el.addEventListener("click", function (ev) {
-					var _iteratorNormalCompletion2 = true;
-					var _didIteratorError2 = false;
-					var _iteratorError2 = undefined;
+        _el.addEventListener("click", function (ev) {
+          var _iteratorNormalCompletion2 = true;
+          var _didIteratorError2 = false;
+          var _iteratorError2 = undefined;
 
-					try {
+          try {
 
-						for (var _iterator2 = _btns[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-							var _e = _step2.value;
+            for (var _iterator2 = _btns[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+              var _e = _step2.value;
 
-							_e.classList.remove('is-active');
-						}
-					} catch (err) {
-						_didIteratorError2 = true;
-						_iteratorError2 = err;
-					} finally {
-						try {
-							if (!_iteratorNormalCompletion2 && _iterator2.return) {
-								_iterator2.return();
-							}
-						} finally {
-							if (_didIteratorError2) {
-								throw _iteratorError2;
-							}
-						}
-					}
+              _e.classList.remove('is-active');
+            }
+          } catch (err) {
+            _didIteratorError2 = true;
+            _iteratorError2 = err;
+          } finally {
+            try {
+              if (!_iteratorNormalCompletion2 && _iterator2.return) {
+                _iterator2.return();
+              }
+            } finally {
+              if (_didIteratorError2) {
+                throw _iteratorError2;
+              }
+            }
+          }
 
-					ev.currentTarget.classList.add('is-active');
-				});
-			}
-		} catch (err) {
-			_didIteratorError = true;
-			_iteratorError = err;
-		} finally {
-			try {
-				if (!_iteratorNormalCompletion && _iterator.return) {
-					_iterator.return();
-				}
-			} finally {
-				if (_didIteratorError) {
-					throw _iteratorError;
-				}
-			}
-		}
-	};
-	/*
- * CALLBACK :: end
- * ============================================= */
+          ev.currentTarget.classList.add('is-active');
+        });
+      }
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+          _iterator.return();
+        }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
+    }
+  };
+  /*
+  * CALLBACK :: end
+  * ============================================= */
 
-	/**
-  * @name initNative
-  *
-  * @description Init all method
-  */
-	var initNative = function initNative() {
-		// default
-		initWebFontLoader();
-		initPreventBehavior();
-		initSvg4everybody();
-		// ==========================================
+  /**
+   * @name initNative
+   *
+   * @description Init all method
+   */
+  var initNative = function initNative() {
+    // default
+    initWebFontLoader();
+    initPreventBehavior();
+    initSvg4everybody();
+    // ==========================================
 
-		// lib
-		// ==========================================
+    // lib
+    // ==========================================
+    initHamburger();
 
-		// callback
-		// ==========================================
-		initPricingTabs();
-	};
-	initNative();
+    // callback
+    // ==========================================
+    initPricingTabs();
+  };
+  initNative();
 })();
